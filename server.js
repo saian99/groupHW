c61e5ab362c252c57314c5c8931ed7db05b43fad
Moath <moath.dlaii@gmail.com>
2020-06-16 18:32:18 -0700

    setup server.js file

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const app = express();
 
+app.use(express.json())
+app.use(express.static('public'))
 
-app.get('/', (req, res) =>{
-  res.send('Hello World')
-})
+
+const moviesController = require('./controllers/movies.js')
+app.use('/movies', moviesController)
+
+// favicon.ico error
+app.use( function(req, res, next) {
+
+  if (req.originalUrl && req.originalUrl.split("/").pop() === 'favicon.ico') {
+    return res.sendStatus(204);
+  }
+
+  return next();
+
+});
+
+mongoose.connect('mongodb://localhost:27017/moviesmeancrud', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', ()=>{
+    console.log('connected to mongod...');
+});
+
 
 
 app.listen(3000,() =>{
